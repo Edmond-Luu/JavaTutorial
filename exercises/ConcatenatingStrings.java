3d75cfbae3ca6f6d3ecc5314e9d4a8fbe9bce96e
Edmond Luu <eluu472@gmail.com>
2022-12-15 11:02:26 -0800

    ConcatenatingStrings

--- exercises/ConcatenatingStrings.java
+++ exercises/ConcatenatingStrings.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
+
 public class ConcatenatingStrings {
     public static void main(String[] args) {
         double studentGPA = 3.45;
         String studentFirstName = "Edmond";
         String studentLastName = "Luu";
 
-        System.out.print(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA);
+
+        System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA);
+        
+        
+        // If we want to allow the user to update the GPA:
+        // Must import java.util.Scanner first.
+        System.out.println("What do you want to update it to?");
+        Scanner input = new Scanner(System.in);
+        studentGPA = input.nextDouble();
+        
+        System.out.println(studentFirstName + " " + studentLastName + " now has a GPA of " + studentGPA);
     }
 }
