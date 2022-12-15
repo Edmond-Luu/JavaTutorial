f56062e513d28c4c81ea8fd56c8b75f61b055a42
Edmond Luu <eluu472@gmail.com>
2022-12-15 10:46:12 -0800

    Student

---
+++ exercises/Student.java
@@ -0,0 +1,15 @@
+public class Student {
+    public static void main(String[] args) {
+        int studentAge = 20;
+        double studentGPA = 3.45;
+        char studentFirstInitial = 'E';
+        char studentLastInitial = 'L';
+        boolean hasPerfectAttendance = true;
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+        System.out.println(hasPerfectAttendance);
+    }
+}
