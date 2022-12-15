f040c618d4474ab6c256ac93da7d7f3a17a7bb6b
Edmond Luu <eluu472@gmail.com>
2022-12-15 10:57:05 -0800

    Concatenating strings

---
+++ exercises/ConcatenatingStrings.java
@@ -0,0 +1,9 @@
+public class ConcatenatingStrings {
+    public static void main(String[] args) {
+        double studentGPA = 3.45;
+        String studentFirstName = "Edmond";
+        String studentLastName = "Luu";
+
+        System.out.print(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA);
+    }
+}
