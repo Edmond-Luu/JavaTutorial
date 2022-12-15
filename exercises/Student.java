a9855bec045f046ea4258edb14f0cf5db20997c3
Edmond Luu <eluu472@gmail.com>
2022-12-15 10:53:00 -0800

    Student

--- exercises/Student.java
+++ exercises/Student.java
@@ -8,23 +8,26 @@ public class Student {
         char studentLastInitial = 'L';
         boolean hasPerfectAttendance = true;
 
-
-
-
+        
         // Reference type: string
         // String MUST use double quotes:" "
         String studentFirstName = "Edmond";
         String studentLastName = "Luu";
 
 
+        // Using indexes for string: use charAt
+        char firstInitial = studentFirstName.charAt(0);
+        char lastInitial = studentLastName.charAt(0);
 
 
         System.out.println(studentAge);
         System.out.println(studentGPA);
-        System.out.println(studentFirstInitial);
-        System.out.println(studentLastInitial);
+        // System.out.println(studentFirstInitial);
+        // System.out.println(studentLastInitial);
         System.out.println(hasPerfectAttendance);
         System.out.println(studentFirstName);
         System.out.println(studentLastName);
+        System.out.println(firstInitial);
+        System.out.println(lastInitial);
     }
 }
