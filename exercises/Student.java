5b2bb3c68516f3def04a587f9199883bab75c919
Edmond Luu <eluu472@gmail.com>
2022-12-15 10:49:39 -0800

    Student

--- exercises/Student.java
+++ exercises/Student.java
@@ -4,14 +4,19 @@ public class Student {
         int studentAge = 20;
         double studentGPA = 3.45;
         char studentFirstInitial = 'E';
-        // char (single letters) MUST use single quotes: ''
+        // char (single letters) MUST use single quotes: ' '
         char studentLastInitial = 'L';
         boolean hasPerfectAttendance = true;
+
+        // Reference type: string
+        // String MUST use double quotes:" "
+        String studentName = "Edmond Luu";
 
         System.out.println(studentAge);
         System.out.println(studentGPA);
         System.out.println(studentFirstInitial);
         System.out.println(studentLastInitial);
         System.out.println(hasPerfectAttendance);
+        System.out.println(studentName);
     }
 }
