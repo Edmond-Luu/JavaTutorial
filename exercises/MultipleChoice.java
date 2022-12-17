728a854ddcc224be25840290c7f7050f91c81adf
Edmond Luu <eluu472@gmail.com>
2022-12-17 10:57:48 -0800

    Multiple choice

--- exercises/MultipleChoice.java
+++ exercises/MultipleChoice.java
@@ -7,7 +7,7 @@ public class MultipleChoice {
         String question = "Which of these LoL champions did NOT appear in Arcane?";
         String choiceOne = "Viktor";
         String choiceTwo = "Jayce";
-        String choiceThree = "Master Yi";
+        String choiceThree = "Teemo";
 
         String correctAnswer = choiceThree;
 
@@ -17,7 +17,9 @@ public class MultipleChoice {
         String userInput = input.next();
         String capitalisedInput = userInput.substring(0, 1).toUpperCase() + userInput.substring(1).toLowerCase();
 
-        if (capitalisedInput == correctAnswer) {
+        System.out.println(capitalisedInput);
+
+        if (capitalisedInput.equals(correctAnswer)) {
             System.out.println("Congrats! The answer is correct!");
         } else {
             System.out.println("The answer was incorrect. The correct answer is" + " " + correctAnswer);
