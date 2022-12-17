577aaf370a110df10458451c87e504aca167f4f0
Edmond Luu <eluu472@gmail.com>
2022-12-17 10:42:26 -0800

    MultipleChoice

--- exercises/FortuneTeller.java
+++ exercises/FortuneTeller.java
@@ -5,7 +5,7 @@ public class FortuneTeller {
         System.out.println("Pick a number betweem 1 and 10");
         Scanner scanner = new Scanner(System.in);
         int inputtedNum = scanner.nextInt();
-        if(inputtedNum < 5){
+        if (inputtedNum < 5) {
             System.out.println("Good luck");
         } else {
             System.out.println("Happiness");
---
+++ exercises/MultipleChoice.java
@@ -0,0 +1,10 @@
+public class MultipleChoice {
+    public static void main(String[] args) {
+        String question = "";
+        String choiceOne = "";
+        String choiceTwo = "";
+        String choiceThree = "";
+
+        String correctAnswer = choiceThree;
+    }
+}
