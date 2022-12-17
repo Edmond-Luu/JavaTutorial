e97f9aa8ea99b1e8848add7ad9babf06366c7601
Edmond Luu <eluu472@gmail.com>
2022-12-17 10:37:25 -0800

    WhileLoops

--- exercises/WhileLoops.java
+++ exercises/WhileLoops.java
@@ -4,5 +4,15 @@ public class WhileLoops {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         boolean isOnRepeat = true;
+        while (isOnRepeat) {
+            System.out.println("Playing current song");
+            System.out.println("Stop repeating?");
+            String userInput = input.next();
+
+            if (userInput.equals("yes")) {
+                isOnRepeat = false;
+            }
+        }
+        System.out.println("Playing next song.");
     }
 }
