e6c7f648db0d22cbf91e2eaa19ca46075c96c5e8
Edmond Luu <eluu472@gmail.com>
2022-12-17 10:30:05 -0800

    FortuneTeller

---
+++ exercises/FortuneTeller.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class FortuneTeller {
+    public static void main(String[] args) {
+        System.out.println("Pick a number betweem 1 and 10");
+        Scanner scanner = new Scanner(System.in);
+        int inputtedNum = scanner.nextInt();
+        if(inputtedNum < 5){
+            System.out.println("Good luck");
+        } else {
+            System.out.println("Happiness");
+        }
+    }
+}
