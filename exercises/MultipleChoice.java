260b8a79279bd835121f61c0399b8a204628184d
Edmond Luu <eluu472@gmail.com>
2022-12-17 12:01:51 -0800

    Multiple choice

--- exercises/MultipleChoice.java
+++ exercises/MultipleChoice.java
@@ -4,25 +4,25 @@ public class MultipleChoice {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        String question = "Which of these LoL champions did NOT appear in Arcane?";
+        String question = "Which of these LoL champions did NOT appear in the Arcane TV series?";
         String choiceOne = "Viktor";
         String choiceTwo = "Jayce";
         String choiceThree = "Teemo";
+        String choiceFour = "Ekko";
 
         String correctAnswer = choiceThree;
 
         System.out.println(question);
-        System.out.println("Choose one of the following: " + choiceOne + ", " + choiceTwo + ", " + choiceThree);
+        System.out.println("Choose one of the following: " + choiceOne + ", " + choiceTwo + ", " + choiceThree + ", "
+                + choiceFour + ".");
 
         String userInput = input.next();
         String capitalisedInput = userInput.substring(0, 1).toUpperCase() + userInput.substring(1).toLowerCase();
 
-        System.out.println(capitalisedInput);
-
         if (capitalisedInput.equals(correctAnswer)) {
             System.out.println("Congrats! The answer is correct!");
         } else {
-            System.out.println("The answer was incorrect. The correct answer is" + " " + correctAnswer);
+            System.out.println("The answer was incorrect. The correct answer is" + " " + correctAnswer + ".");
         }
 
     }
