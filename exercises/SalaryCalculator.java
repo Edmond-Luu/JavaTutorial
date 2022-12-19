e4c02cde12b6333f513009c46f8e94583ebad9da
Edmond Luu <eluu472@gmail.com>
2022-12-19 15:16:19 -0800

    SalaryCalculator

--- exercises/SalaryCalculator.java
+++ exercises/SalaryCalculator.java
@@ -1,11 +1,36 @@
+import java.util.Scanner;
+
 public class SalaryCalculator {
-    public static double calculateSalary(double hoursPerWeek, double payRate, int vacationDays) {
+    public static double calculateSalary(double hoursPerWeek, double payRate, double vacationDays) {
         double weeklySalary = hoursPerWeek * payRate;
         double yearlySalary = (weeklySalary * 52) - (vacationDays * 8 * payRate);
         return yearlySalary;
     }
 
     public static void main(String[] args) {
-        System.out.println(calculateSalary(40, 30, 10));
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter the hours worked per week: ");
+        double hours = input.nextDouble();
+        while (hours < 0) {
+            System.out.println("Sorry, invalid response. Please enter the hours worked per week: ");
+            hours = input.nextDouble();
+        }
+
+        System.out.print("Please enter the hourly pay rate: ");
+        double pay = input.nextDouble();
+        while (pay <= 0) {
+            System.out.println("Sorry, invalid response. Please enter the hourly pay rate: ");
+            pay = input.nextDouble();
+        }
+
+        System.out.print("Please enter the total vacation days: ");
+        double vacation = input.nextDouble();
+        while (vacation < 0) {
+            System.out.println("Sorry, invalid response. Please enter the total vacation days: ");
+            vacation = input.nextDouble();
+        }
+
+        System.out.println("The total yearly salary is $" + calculateSalary(hours, pay, vacation));
     }
 }
