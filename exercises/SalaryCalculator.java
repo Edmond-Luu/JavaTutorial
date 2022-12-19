41c00d98650eced8c1264eb21c460db22242015b
Edmond Luu <eluu472@gmail.com>
2022-12-19 15:04:24 -0800

    SalaryCalculator

--- exercises/SalaryCalculator.java
+++ exercises/SalaryCalculator.java
@@ -1,5 +1,10 @@
 public class SalaryCalculator {
+    public static double calculateSalary(double hoursPerWeek, double payRate) {
+        double weeklySalary = hoursPerWeek * payRate;
+        return weeklySalary * 52;
+    }
+
     public static void main(String[] args) {
-        
+        System.out.println(calculateSalary(40, 30));
     }
 }
