8e04d4ea7e475ca807b5d02383422699432916c8
Edmond Luu <eluu472@gmail.com>
2022-12-19 15:06:29 -0800

    SalaryCalculator

--- exercises/SalaryCalculator.java
+++ exercises/SalaryCalculator.java
@@ -1,10 +1,11 @@
 public class SalaryCalculator {
-    public static double calculateSalary(double hoursPerWeek, double payRate) {
+    public static double calculateSalary(double hoursPerWeek, double payRate, int vacationDays) {
         double weeklySalary = hoursPerWeek * payRate;
-        return weeklySalary * 52;
+        double yearlySalary = (weeklySalary * 52) - (vacationDays * 8 * payRate);
+        return yearlySalary;
     }
 
     public static void main(String[] args) {
-        System.out.println(calculateSalary(40, 30));
+        System.out.println(calculateSalary(40, 30, 1));
     }
 }
