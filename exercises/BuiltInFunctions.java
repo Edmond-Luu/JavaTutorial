70a051f44062ff4a3950995c8e3baf829e92fb90
Edmond Luu <eluu472@gmail.com>
2022-12-19 14:59:51 -0800

    BuiltInFunctions

--- exercises/BuiltInFunctions.java
+++ exercises/BuiltInFunctions.java
@@ -1,5 +1,6 @@
 public class BuiltInFunctions {
     public static void main(String[] args) {
-        
+        // Math.pow is a built-in function that calculates exponents.
+        System.out.println(Math.pow(2, 3));
     }
 }
