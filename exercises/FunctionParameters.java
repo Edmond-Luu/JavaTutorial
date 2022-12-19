b70d98806dd509807f087f00042db900b92b0343
Edmond Luu <eluu472@gmail.com>
2022-12-19 14:47:19 -0800

    FunctionParameters

--- exercises/FunctionParameters.java
+++ exercises/FunctionParameters.java
@@ -1,4 +1,12 @@
 public class FunctionParameters {
+    // Like in JS, put the parameters in the function definition's parenthesis.
+    public static void calculateTotalMealPrice(double listedMealPrice, double tipRate, double taxRate) {
+        double tip = tipRate * listedMealPrice;
+        double tax = tipRate * listedMealPrice;
+        double result = listedMealPrice + tip + tax;
+        System.out.println("Your total meal price is " + result);
+    }
+
     public static void main(String[] args) {
 
     }
