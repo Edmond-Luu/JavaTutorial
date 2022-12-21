4693a0d397784a400322193bed6145a343f99eb5
Edmond Luu <eluu472@gmail.com>
2022-12-21 15:38:15 -0800

    StudentProfile

--- exercises/StudentProfileMain.java
+++ exercises/StudentProfileMain.java
@@ -1,7 +1,9 @@
 public class StudentProfileMain {
     public static void main(String[] args) {
+        StudentProfile jane = new StudentProfile("Jane", "Doe", 2022, 3.7, "Computer Engineering");
         StudentProfile john = new StudentProfile("John", "Doe", 2022, 3.5, "Computer Science");
         john.addYear();
         System.out.println(john.gradYear);
+        System.out.println(jane.gpa);
     }
 }
