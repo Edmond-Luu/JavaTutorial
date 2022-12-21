14d18dedcd99334f4dc8047361302f993196591f
Edmond Luu <eluu472@gmail.com>
2022-12-20 16:05:00 -0800

    Classes

---
+++ exercises/Classes.java
@@ -0,0 +1,13 @@
+public class Classes {
+    public class Triangle {
+        double base;
+        double height;
+        double sideLenOne;
+        double sideLenTwo;
+        double sideLenThree;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
